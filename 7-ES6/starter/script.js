d85337a4c7113915e269cd2789bf8d383710e3de
Franklin Wu <franklin.w.wu@gmail.com>
2018-09-22 19:00:48 -0700

    Correct coding mini-challenge 7-a

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -16,9 +16,7 @@ Person.prototype.myFriends5 = function(friends) {
 */
 
 Person.prototype.myFriends5 = function(friends) {
-    var arr = friends.map((el) => {
-       return this.name + ' is friends with ' + el;
-    });
+    var arr = friends.map(el => this.name + ' is friends with ' + el);
     console.log(arr);
 };
 

9eabb765802e07d4bc9bbc887359edc39e865e42
Franklin Wu <franklin.w.wu@gmail.com>
2018-09-25 01:41:30 -0700

    Correct coding challenge 7-5

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -43,6 +43,13 @@ All the report data should be printed to the console.
 HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
 */
 
+sizeMap = new Map();
+sizeMap.set(1, 'tiny');
+sizeMap.set(2, 'small');
+sizeMap.set(3, 'normal');
+sizeMap.set(4, 'big');
+sizeMap.set(5, 'huge');
+
 class Element {
     constructor(name, establishmentYear, length) {
         this.name = name;
@@ -68,72 +75,53 @@ class Park extends Element {
 }
 
 class Street extends Element {
-    constructor(name, establishmentYear, length, size = 'normal') {
+    constructor(name, establishmentYear, length, size = 3) {
         super(name, establishmentYear, length);
         this.size = size;
     }
 
 }
 
-const elements = [];
-elements.push(new Park('Central Park', 1909, 10.02, 4.69, 1501));
-elements.push(new Park('Bark Park', 1999, 0.35, 0.15, 11));
-elements.push(new Park('Park of Wonder', 1954, 2.34, 4.53, 198));
-elements.push(new Street('Main Street', 1899, 18.08, 'huge'));
-elements.push(new Street('Oak Street', 1925, 7.44));
-elements.push(new Street('Wonder Way', 1945, 20.34, 'big'));
-elements.push(new Street('Fern Way', 1975, 1.34, 'tiny'));
-// console.log(elements);
+const calculateSumAndAverage = (elements) => {
+    const sum = elements.reduce((accumulator, current) => accumulator + current);
+    return [sum, sum / elements.length];
+};
+
+const parks = [];
+parks.push(new Park('Central Park', 1909, 10.02, 4.69, 1501));
+parks.push(new Park('Bark Park', 1999, 0.35, 0.15, 11));
+parks.push(new Park('Park of Wonder', 1954, 2.34, 4.53, 198));
+
+const streets = [];
+streets.push(new Street('Main Street', 1899, 18.08, 5));
+streets.push(new Street('Oak Street', 1925, 7.44));
+streets.push(new Street('Wonder Way', 1945, 20.34, 4));
+streets.push(new Street('Fern Way', 1975, 1.34, 1));
 
 // 1. Tree density of each park in the town (forumla: number of trees/park area)
-elements.forEach((element) => {
-    if (element instanceof Park) {
-        const treeDensity = element.getTreeDensity().toFixed(2);
-        console.log(`${element.name} has a tree density of ${treeDensity} trees per square mile.`);
-    }
+parks.forEach((park) => {
+    const treeDensity = park.getTreeDensity().toFixed(2);
+    console.log(`${park.name} has a tree density of ${treeDensity} trees per square mile.`);
 });
 
 // 2. Average age of each town's park (forumla: sum of all ages/number of parks)
 console.log('\n');
-const ageMap = new Map();
-ageMap.set('aggregateAge', 0);
-ageMap.set('parkCount', 0);
-for (let element of elements) {
-    if (element instanceof Park) {
-        ageMap.set('aggregateAge', ageMap.get('aggregateAge') + element.getAge());
-        ageMap.set('parkCount', ageMap.get('parkCount') + 1);
-    }
-}
-const aveageAge = ageMap.get('aggregateAge') / ageMap.get('parkCount');
-console.log(`The average age of the town's parks is ${aveageAge} years.`);
+ages = parks.map(park => park.getAge());
+[_, average] = calculateSumAndAverage(ages);
+console.log(`The average age of the town's parks is ${average.toFixed(2)} years.`);
 
 // 3. The name of the park that has more than 1000 trees
 console.log('\n');
-for (let element of elements) {
-    if (element instanceof Park && element.treeCount > 1000) {
-        console.log(`${element.name}, with ${element.treeCount} trees, has more than 1000 trees.`)
-    }
-}
+const park = parks.find(park => park.treeCount > 1000);
+console.log(`${park.name}, with ${park.treeCount} trees, has more than 1000 trees.`)
 
 // 4. Total and average length of the town's streets
 console.log('\n');
-const lengthMap = new Map();
-lengthMap.set('aggregateLength', 0);
-lengthMap.set('streetCount', 0);
-for (let element of elements) {
-    if (element instanceof Street) {
-        lengthMap.set('aggregateLength', lengthMap.get('aggregateLength') + element.length);
-        lengthMap.set('streetCount', lengthMap.get('streetCount') + 1);
-    }
-}
-const averageLength = (lengthMap.get('aggregateLength') / lengthMap.get('streetCount')).toFixed(2);
-console.log(`The total length of the town's streets is ${lengthMap.get('aggregateLength')} miles.`);
-console.log(`The average length of the town's streets is ${averageLength} miles.`);
+lengths = streets.map(street => street.length);
+[sum, average] = calculateSumAndAverage(lengths);
+console.log(`The total length of the town's streets is ${sum.toFixed(2)} miles.`);
+console.log(`The average length of the town's streets is ${average.toFixed(2)} miles.`);
 
 // 5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
 console.log('\n');
-elements.forEach((element) => {
-    if (element instanceof Street) {
-        console.log(`${element.name} has a size classification of ${element.size}.`);
-    }
-});
+streets.forEach(street => console.log(`${street.name} has a size classification of ${sizeMap.get(street.size)}.`));
